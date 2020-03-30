45477f80a1078754ca0fc4cd84be6335d247baa1
Destiny Malone <dmalone@basecampcodingacademy.org>
2020-03-30 11:16:56 -0500

    Urls for list/card detail. Don't know if they need id's.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path,  include
 from django.contrib.auth import views as auth_views
-from polls.views import (RegistrationView, CreateBoardView, UserDetailView, UserBoardList, UserListCard)
+from polls.views import (RegistrationView, CreateBoardView, UserDetailView, UserBoardList, UserListCard, UserListDetail, UserCardDetail)
 
 
 
@@ -10,8 +10,10 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/register/", RegistrationView.as_view()),
     path("board/", CreateBoardView.as_view()),
-    path("detail<id>/", UserDetailView.as_view()),
+    path("board/detail<id>/", UserDetailView.as_view()),
     path("list/", UserBoardList.as_view()),
-    path("list/", UserListCard.as_view()),
+    path("list/detail", UserListDetail.as_view()),
+    path("card/", UserListCard.as_view()),
+    path("card/detail", UserCardDetail.as_view()),
     path('polls/', include('polls.urls')),
 ]
